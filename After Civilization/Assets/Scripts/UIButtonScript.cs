4db545b2e18dd4db5f7d03041925b7e983e74703
Svanurjoh <svanurjoh15@ru.is>
2018-03-20 20:22:41 +0000

    UI

---
+++ After Civilization/Assets/Scripts/UIButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtonScript : MonoBehaviour {
+
+	public void NewGameButton()
+	{
+		SceneManager.LoadScene ("Main");
+	}
+
+	public void ControlsButton(string controls)
+	{}
+
+	public void ExitButton()
+	{
+		Application.Quit ();
+	}
+}
