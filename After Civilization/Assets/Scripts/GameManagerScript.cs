191143562a56bbf6474ee5ca453112565c0db74c
Svanurjoh <svanurjoh15@ru.is>
2018-03-20 16:23:26 +0000

    Dno works

--- After Civilization/Assets/Scripts/GameManagerScript.cs
+++ After Civilization/Assets/Scripts/GameManagerScript.cs
@@ -6,6 +6,7 @@ public class GameManagerScript : MonoBehaviour {
 
 	public GameObject[] gemSpawns;
 	public GameObject[] enemySpawns;
+	public GameObject[] enemyMassSpawns;
 	public GameObject redGemPrefab;
 	public GameObject enemyPrefab;
 
@@ -15,7 +16,13 @@ public class GameManagerScript : MonoBehaviour {
 			Instantiate (redGemPrefab, gemSpawns [i].transform.position, redGemPrefab.transform.rotation);
 		}
 		for (int i = 0; i < 20; i++) {
-			Instantiate (enemyPrefab, enemySpawns [0].transform.position, enemyPrefab.transform.rotation);
+			Instantiate (enemyPrefab, enemyMassSpawns [0].transform.position, enemyPrefab.transform.rotation);
+		}
+		for (int i = 0; i < 5; i++) {
+			Instantiate (enemyPrefab, enemyMassSpawns [1].transform.position, enemyPrefab.transform.rotation);
+		}
+		for (int i = 0; i < enemySpawns.Length; i++) {
+			Instantiate (enemyPrefab, enemySpawns [i].transform.position, enemyPrefab.transform.rotation);
 		}
 	}
 	
